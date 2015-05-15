4a222d1a6fb60ff3fb638979fb03904a1c79f407
Elena Bojilova <elena_bogoeva2003@yahoo.co.uk>
2015-05-15 22:54:20 +0300

    chaned 06

--- 06.ExtractHyperlinks/ExtractHyperlinks.cs
+++ 06.ExtractHyperlinks/ExtractHyperlinks.cs
@@ -16,13 +16,15 @@ class ExtractHyperlinks
         string text = sb.ToString();
         // <a[^>]* href="([^"]*)"
         //<a[^>]* href\s*=\s*(["']*)([^""]*?)\1  -s tozi pattern mi dava 33 tochki v Juge, no edinia nulev ne minava
-        string pattern = "<a[^>]* href\\s*=\\s*(\"(#[a-z]*)\"|\"([^\"]*?)\" |\'([^\"]*?)\'|([a-z:./]+))";
+       // <a[^>]* href\\s*=\\s*((#[a-z]*)|\"([^\"]*?)\"|'([^']*?)'|([a-z:.]+))
+        //<a\\s+([^>]+\\s+)?href\\s*=\\s*('([^']*)'|\"([^\"]*)|([^\\s>]+))[^>]*>
+        string pattern = @"<a\s+(?:[^>]+\s+)?href\s*=\s*(?:'([^']*)'|""([^""]*)""|([^\s>]+))[^>]*>";
         Regex users = new Regex(pattern);
         MatchCollection matches = users.Matches(text);
         //Console.WriteLine("Found {0} matches", matches.Count);
         foreach (Match hyperlink in matches)
         {
-            for (int i = 2; i <= 5; i++)
+            for (int i = 1; i <= 3; i++)
             {
                 if (hyperlink.Groups[i].Length>0)
                 {
