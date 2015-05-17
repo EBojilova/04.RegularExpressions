8a864630d8686e87d729675037a19ec021a6d45e
Elena Bojilova <elena_bogoeva2003@yahoo.co.uk>
2015-05-17 18:00:54 +0300

    09 update

--- 09.SemanticHTMLInnos/09.SemanticHTMLInnos.cs
+++ 09.SemanticHTMLInnos/09.SemanticHTMLInnos.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace _09.SemanticHTMLInnos
+class SemanticHTML
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        StringBuilder sb = new StringBuilder();
+        string input;
+        string patternOpen = @"(?:id|class)\s*=\s*""(?<result>[^\s]+)""";
+        string patternClose = @"<\/div>\s*?<!--\s*?(\w{3,7})\s*?-->";
+        string replaceClose = @"</$1>";
+        while ((input = Console.ReadLine()) != "END")
         {
+            if (input.Contains("/div"))
+            {
+                input=Regex.Replace(input, patternClose, replaceClose);
+            }
+            else if (input.Contains("div"))
+            {
+                string HTMLClass = Regex.Match(input, patternOpen).Groups["result"].Value;
+                input = Regex.Replace(input, patternOpen, "");
+                input = Regex.Replace(input, "div", HTMLClass);
+                input = Regex.Replace(input, @"(?<=<.*)\s+", " ");
+                input = Regex.Replace(input, @"\s+>", ">");
+            }
+            sb.Append(input);
+            sb.Append("\n");
         }
+        string text = sb.ToString();
+        Console.WriteLine(text);
     }
 }
